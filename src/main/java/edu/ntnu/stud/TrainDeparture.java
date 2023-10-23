c517090842578d5bc25e416e942a38dbef97007b
leifssm <leif@moerstad.no>
2023-10-23 12:12:12 +0200

    added check to train departure params

--- src/main/java/edu/ntnu/stud/TrainDeparture.java
+++ src/main/java/edu/ntnu/stud/TrainDeparture.java
@@ -17,11 +17,24 @@ public class TrainDeparture {
         this(plannedDeparture, line, trainNumber, destination, track, LocalTime.MIN);
     }
     TrainDeparture(LocalTime plannedDeparture, String line, int trainNumber, String destination, int track, LocalTime delay) {
+        if (plannedDeparture == null) throw new InvalidParameterException("Planned departure cannot be null");
         this.plannedDeparture = plannedDeparture;
+
+        if (line == null) throw new InvalidParameterException("Line cannot be null");
+        if (line.length() > 3) throw new InvalidParameterException("Line cannot be longer than 3 characters");
+        if (line.length() < 2) throw new InvalidParameterException("Line cannot be shorter than 2 characters");
         this.line = line;
+
+        if (trainNumber < -1) throw new InvalidParameterException("Train number cannot be less than -1");
         this.trainNumber = trainNumber;
+
+        if (destination == null) throw new InvalidParameterException("Destination cannot be null");
         this.destination = destination;
+
+        if (track < 0) throw new InvalidParameterException("Track id cannot be less than 0");
         this.track = track;
+
+        if (delay == null) throw new InvalidParameterException("Delay cannot be null");
         this.delay = delay;
     }
 
@@ -59,6 +72,9 @@ public class TrainDeparture {
         return delay;
     }
 
+    public boolean isDelayed() {
+        return !delay.equals(LocalTime.MIN);
+    }
     public void setDelay(LocalTime delay) {
         this.delay = delay;
     }
