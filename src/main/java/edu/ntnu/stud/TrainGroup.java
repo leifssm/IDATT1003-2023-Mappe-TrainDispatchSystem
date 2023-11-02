e023d2b07791157dc76f63d6689a32502e9276bc
Leif Mørstad <leif@moerstad.no>
2023-11-02 15:27:07 +0100

    added helper functions

--- src/main/java/edu/ntnu/stud/TrainGroup.java
+++ src/main/java/edu/ntnu/stud/TrainGroup.java
@@ -3,30 +3,56 @@ package edu.ntnu.stud;
 import org.jetbrains.annotations.NotNull;
 
 import java.security.InvalidParameterException;
+import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.stream.Stream;
 
 public class TrainGroup {
+
     private final ArrayList<TrainDeparture> departures = new ArrayList<>();
 
     TrainGroup() {}
-    TrainGroup(ArrayList<TrainDeparture> departures) {
+    TrainGroup(@NotNull ArrayList<TrainDeparture> departures) {
         for (TrainDeparture departure : departures) {
             addDeparture(departure);
         }
     }
     public void addDeparture(@NotNull TrainDeparture departure) {
         final int id = departure.getTrainNumber();
-        if (id != -1 && getDeparture(id) != null) throw new InvalidParameterException("Train number already exists");
+        if (id != -1 && getDepartureFromNumber(id) != null) throw new InvalidParameterException("Train number already exists");
         departures.add(departure);
     }
-    private TrainDeparture getDeparture(int trainNumber) {
+    public TrainDeparture getDepartureFromNumber(int trainNumber) {
         return departures
                 .stream()
                 .filter(departure -> departure.getTrainNumber() == trainNumber)
                 .findFirst()
                 .orElse(null);
     }
-    public TrainDeparture[] getDepartures() {
-        return departures.toArray(new TrainDeparture[departures.size()]);
+    public TrainDeparture[] getDepartureFromDestination(@NotNull String destination) {
+        return departures
+                .stream()
+                .filter(departure -> departure.getDestination().equals(destination))
+                .toArray(TrainDeparture[]::new);
     }
+
+    private Stream<TrainDeparture> getSortedDepartureStream() {
+        return departures
+                .stream()
+                .sorted((a, b) -> {
+                    final LocalTime aDeparture = a.getPlannedDeparture();
+                    final LocalTime bDeparture = b.getPlannedDeparture();
+                    return aDeparture.compareTo(bDeparture);
+                });
+    }
+    public TrainDeparture[] getDeparturesFromTime(@NotNull LocalTime time) {
+        return getSortedDepartureStream()
+                .filter(departure -> departure.getDelayedDeparture().isAfter(time))
+//                .limit(LINES)
+                .toArray(TrainDeparture[]::new);
+    }
+    public TrainDeparture[] getDeparturesFromTime() {
+        return getSortedDepartureStream().toArray(TrainDeparture[]::new);
+    }
+
 }
--- src/main/java/edu/ntnu/stud/TrainInterface.java
+++ src/main/java/edu/ntnu/stud/TrainInterface.java
@@ -26,10 +26,39 @@ public class TrainInterface {
     }
 
     public void start() {
-        LocalTime currentTime = LocalTime.of(0,0);
-        showDeparturesFrom(currentTime);
+        LocalTime currentTime = LocalTime.MIN;
+        while (true) {
+            showDepartures(currentTime);
+            System.out.println(
+                    "Velg oppgave:\n" +
+                    "1. Legg til ny togavgang\n" +
+                    "2. Gi togspor til togavgang\n" + // Finn med tognummer og sett spor
+                    "3. Sett forsinkelse\n" + // Finn med tognummer og sett forsinkelse
+                    "4. Finn avgang med nummer\n" + // Finn med tognummer
+                    "5. Finn avgang med destinasjon\n" + // Finn med destinasjon
+                    "6. Sett klokken\n" + // kan ikke være mindre
+                    "7. Avslutt"
+            );
+            final int option = InputParser.getInt("Valg: ", (int n) -> n >= 1 && n <= 7);
+            System.out.println();
+            switch (option) {
+                case 1 -> addDeparture();
+                case 2 -> giveDepartureTrack();
+                case 3 -> giveDepartureDelay();
+                case 4 -> findDepartureFromNumber();
+                case 5 -> findDepartureFromDestination();
+                case 6 -> setClock(currentTime);
+                case 7 -> System.exit(0);
+            };
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ignored) {}
+        }
     }
 
+    private void addDeparture() {
+        // TODO
+    }
 
 
 
@@ -37,18 +66,101 @@ public class TrainInterface {
     private void showDeparturesFrom() {
         showDeparturesFrom(LocalTime.MIN);
     }
-    private void showDeparturesFrom(LocalTime time) {
+
+    private void giveDepartureTrack() {
+        // TODO
+    }
+    private void giveDepartureTrack() {
+        TrainDeparture departure = findDepartureFromNumber();
+        System.out.println("Hvilket spor skal den gå fra?");
+        final int previousTrack = departure.getTrack();
+        final int track = InputParser.getInt("Spor: ", (int n) -> n >= 0);
+        departure.setTrack(track);
+        System.out.println();
+        if (previousTrack != track) System.out.printf("Spor endret fra %s til %s.\n", previousTrack, track);
+        else System.out.printf("Spor var allerede satt til %s, ingen endring gjort.\n", track);
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException ignored) {}
+    }
+
+    private void giveDepartureDelay() {
+        // TODO
+    }
+
+    private void giveDepartureDelay() {
+        TrainDeparture departure = findDepartureFromNumber();
+        final LocalTime previousDelay = departure.getDelay();
+        if (departure.isDelayed()) System.out.printf("Toget er allerede forsinka med %s.\n", previousDelay);
+        System.out.println("Hvor mye vil du endre togets forsinkelse til?");
+        final LocalTime delay = InputParser.getTime("Forsinkelse: ");
+        departure.setDelay(delay);
+        System.out.println();
+        if (previousDelay != delay) System.out.printf("Forsinkelse endret fra %s til %s.\n", previousDelay, delay);
+        else System.out.printf("Forsinkelsen var allerede satt til %s, ingen endring gjort.\n", delay);
+    }
+
+    private void findDepartureFromNumber() {
+        // TODO
+    }
+
+    private TrainDeparture findDepartureFromNumber() {
+        TrainDeparture departure = null;
+        while (departure == null) {
+            System.out.println("Velg en togavgang å gi spor til. Skriv inn tognummeret.");
+            final int trainNumber = InputParser.getInt("Tognummer: ", (int n) -> n >= 0);
+            departure = departures.getDepartureFromNumber(trainNumber);
+            if (departure == null) System.out.printf("Fant ikke toget med nummer %s. Prøv igjen.", trainNumber);
+        }
+        System.out.printf("Fant toget %s.\n", departure);
+        return departure;
+    }
+
+    private void findDepartureFromDestination() {
+        // TODO
+    }
+
+    private void findDepartureFromDestination() {
+        TrainDeparture[] departures = null;
+        while (departures == null || departures.length == 0) {
+            System.out.println("Velg en togavgang å gi spor til. Skriv inn tognummeret.");
+            final String destination = InputParser.getString("Destinasjon: ");
+            departures = this.departures.getDepartureFromDestination(destination);
+            if (departures.length == 0) System.out.printf("Fant ingen tog med destinasjonen %s. Prøv igjen.", destination);
+        }
+        System.out.println("Fant tog:");
+        for (int i = 0; i < departures.length; i++) {
+            System.out.println(i + 1 + ". " + departures[i]);
+        }
+        System.out.println();
+    }
+
+    private void setClock() {
+        // TODO
+    }
+
+    private LocalTime setClock(LocalTime currentTime) {
+        LocalTime newTime = LocalTime.MIN;
+        do {
+            newTime = InputParser.getTime("Skriv inn nytt klokkeslett, klokkeslettet må være større enn det forrige: ");
+        } while (!newTime.isAfter(currentTime));
+        return newTime;
+    }
+
+
+    private void showDepartures(LocalTime time) {
         if (time == null) throw new IllegalArgumentException("Time cannot be null");
-        final TrainDeparture[] departures = getDeparturesFrom(time);
+        final TrainDeparture[] sortedDepartures = departures.getDeparturesFromTime(time);
 
         System.out.println("                ╔════════════╦═══════╗");
         System.out.printf( "                ║ Departures ║ %s ║\n", time);
         System.out.println("╔═════════╤═════╩═══╤══════╤═╩═══════╩════════╤═══════╗");
         System.out.println("║ Planned │ Delayed │ Line │ Destination      │ Train ║");
-        for (var departure : departures) {
+        for (TrainDeparture departure : sortedDepartures) {
             System.out.println(parseDeparture(departure));
         }
-        for (int i = 0; i < LINES - departures.length; i++) {
+        for (int i = 0; i < 5 - sortedDepartures.length; i++) {
             System.out.println("║         │         │      │                  │       ║");
         }
         System.out.println("╚═════════╧═════════╧══════╧══════════════════╧═══════╝");
