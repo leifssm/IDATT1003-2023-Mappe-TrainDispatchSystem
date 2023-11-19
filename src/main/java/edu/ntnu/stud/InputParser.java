7c0db5a18325db0a099b2963dc02dcd5a34a3442
Leif Mørstad <leif@moerstad.no>
2023-11-19 22:32:24 +0100

    improved inputparser

--- src/main/java/edu/ntnu/stud/InputParser.java
+++ src/main/java/edu/ntnu/stud/InputParser.java
@@ -21,6 +21,7 @@ public class InputParser {
     }
     scanner = new Scanner(stream, StandardCharsets.UTF_8);
   }
+
   static void initialize() {
     initialize(System.in);
   }
@@ -37,51 +38,6 @@ public class InputParser {
     scanner = null;
   }
 
-  public static int getInt(String prompt) {
-    initialize();
-
-    while (true) {
-      System.out.print(prompt + ": ");
-      try {
-        return Integer.parseInt(scanner.nextLine());
-      } catch (NumberFormatException ignored) {
-        // Do nothing
-      }
-    }
-  }
-
-  public static int getInt(String prompt, InputValidator<Integer> validator) {
-    int result;
-    while (true) {
-      result = getInt(prompt);
-      if (validator.test(result)) {
-        return result;
-      }
-    }
-  }
-
-  public static float getFloat(String prompt) {
-    initialize();
-    while (true) {
-      System.out.print(prompt + ": ");
-      try {
-        return Float.parseFloat(scanner.nextLine());
-      } catch (NumberFormatException ignored) {
-        // Do nothing
-      }
-    }
-  }
-
-  public static float getFloat(String prompt, InputValidator<Float> validator) {
-    float result;
-    while (true) {
-      result = getFloat(prompt);
-      if (validator.test(result)) {
-        return result;
-      }
-    }
-  }
-
   public static String getString(String prompt) {
     initialize();
     System.out.print(prompt + ": ");
@@ -89,79 +45,180 @@ public class InputParser {
   }
 
   public static String getString(String prompt, @NonNls @NotNull String regex) {
-    initialize();
+    return getString(prompt, regex, "Strengen passer ikke kriteriene");
+  }
+
+  public static String getString(
+      String prompt,
+      @NonNls @NotNull String regex,
+      String errorMessage
+  ) {
     final Pattern pattern = Pattern.compile(regex);
+    return getString(prompt, pattern, errorMessage);
+  }
+
+  public static String getString(String prompt, Pattern pattern) {
+    return getString(prompt, pattern, "Strengen passer ikke kriteriene");
+  }
+
+  public static String getString(String prompt, Pattern pattern, String errorMessage) {
+    Matcher result;
     while (true) {
-      final Matcher t = pattern.matcher(getString(prompt));
-      if (t.matches()) {
-        return t.group();
+      result = pattern.matcher(getString(prompt));
+      if (result.matches()) {
+        return result.group();
+      }
+      if (errorMessage != null) {
+        System.out.println(errorMessage);
       }
     }
   }
 
-  public static String getString(String prompt, Pattern pattern) {
-    initialize();
+  public static int getInt(String prompt) {
     while (true) {
-      final Matcher t = pattern.matcher(getString(prompt));
-      if (t.matches()) {
-        return t.group();
+      try {
+        return Integer.parseInt(getString(prompt));
+      } catch (NumberFormatException ignored) {
+        System.out.println("Verdien er ikke et gyldig tall");
+      }
+    }
+  }
+
+  public static int getInt(String prompt, InputValidator<Integer> validator) {
+    return getInt(prompt, validator, "Heltallet passer ikke kriteriene");
+  }
+
+  public static int getInt(String prompt, InputValidator<Integer> validator, String errorMessage) {
+    int result;
+    while (true) {
+      result = getInt(prompt);
+      if (validator.test(result)) {
+        return result;
+      }
+      if (errorMessage != null) {
+        System.out.println(errorMessage);
+      }
+    }
+  }
+
+  public static float getFloat(String prompt) {
+    while (true) {
+      try {
+        return Float.parseFloat(getString(prompt));
+      } catch (NumberFormatException ignored) {
+        System.out.println("Verdien er ikke et gyldig desimaltall");
+      }
+    }
+  }
+
+  public static float getFloat(String prompt, InputValidator<Float> validator) {
+    return getFloat(prompt, validator, "Desimaltallet passer ikke kriteriene");
+  }
+
+  public static float getFloat(
+      String prompt,
+      InputValidator<Float> validator,
+      String errorMessage
+  ) {
+    float result;
+    while (true) {
+      result = getFloat(prompt);
+      if (validator.test(result)) {
+        return result;
+      }
+      if (errorMessage != null) {
+        System.out.println(errorMessage);
       }
     }
   }
 
   public static char getChar(String prompt) {
-    initialize();
     String input;
     while (true) {
-      input = getString(prompt);
+      input = getString(prompt, "^.$", null);
       if (input.length() == 1) {
         return input.charAt(0);
       }
+      System.out.println("Du kan ikke gi flere tegn");
     }
   }
 
   public static char getChar(String prompt, InputValidator<Character> validator) {
-    initialize();
-    String input;
+    return getChar(prompt, validator, "Tegnet passer ikke kriteriene");
+  }
+
+  public static char getChar(
+      String prompt,
+      InputValidator<Character> validator,
+      String errorMessage
+  ) {
+    char input;
     while (true) {
-      input = getString(prompt);
-      if (input.length() == 1 && validator.test(input.charAt(0))) {
-        return input.charAt(0);
+      input = getChar(prompt);
+      if (validator.test(input)) {
+        return input;
+      }
+      if (errorMessage != null) {
+        System.out.println(errorMessage);
       }
     }
   }
 
   public static LocalTime getTime(String prompt) {
-    initialize();
     final Pattern pattern = Pattern.compile("^[0-2]?[0-9]:[0-5][0-9]$");
+    String result;
     while (true) {
-      final String result = getString(prompt, pattern);
-      final String paddedResult = result.length() == 4 ? "0" + result : result;
+      result = getString(
+          prompt,
+          pattern,
+          "Vennligst skriv inn tiden på formatet \"h:mm\" eller \"hh:mm\""
+      );
+      if (result.length() == 4) {
+        result = "0" + result;
+      };
       try {
-        return LocalTime.parse(paddedResult);
+        return LocalTime.parse(result);
       } catch (DateTimeParseException ignored) {
-        // Do nothing
+        System.out.println("Tiden er ikke gyldig");
+      }
+    }
+  }
+
+  public static LocalTime getTime(
+      String prompt,
+      InputValidator<LocalTime> validator,
+      String errorMessage
+  ) {
+    LocalTime result;
+    while (true) {
+      result = getTime(prompt);
+      if (validator.test(result)) {
+        return result;
+      }
+      if (errorMessage != null) {
+        System.out.println(errorMessage);
       }
     }
   }
 
   public static LocalTime getTime(String prompt, InputValidator<LocalTime> validator) {
-    while (true) {
-      final LocalTime result = getTime(prompt);
-      if (validator.test(result)) {
-        return result;
-      }
-    }
+    return getTime(prompt, validator, "Tiden passer ikke kriteriene");
   }
 
   public static boolean getBoolean(String prompt) {
-    initialize();
-    return getString(prompt + " [y/n]", "^[ynYN]$").equalsIgnoreCase("y");
+    return getString(
+        prompt + " [y/n]",
+        "^[ynYN]$",
+        "Vennligst bare bruk ett av tegnene gitt"
+    ).equalsIgnoreCase("y");
   }
 
   public static boolean getBoolean(String prompt, boolean defaultValue) {
-    initialize();
-    final String result = getString(prompt + (defaultValue ? " [Y/n]" : " [y/N]"), "^[ynYN]?$");
+    final String result = getString(
+        prompt + (defaultValue ? " [Y/n]" : " [y/N]"),
+        "^[ynYN]?$",
+        "Vennligst bare bruk ett av tegnene gitt"
+    );
     if (result.isEmpty()) {
       return defaultValue;
     }
