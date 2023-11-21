98a263ddfe3cdaac430241c08160457656466a2c
Leif Mørstad <leif@moerstad.no>
2023-11-21 15:10:13 +0100

    improved train departure and train group testability

--- src/test/java/edu/ntnu/stud/TrainDepartureTest.java
+++ src/test/java/edu/ntnu/stud/TrainDepartureTest.java
@@ -3,8 +3,6 @@ package edu.ntnu.stud;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import java.security.InvalidParameterException;
 import java.time.LocalTime;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -12,9 +10,17 @@ import static org.junit.jupiter.api.Assertions.*;
 class TrainDepartureTest {
 
   private TrainDeparture departure;
+
   @BeforeEach
   void setUp() {
-    departure = new TrainDeparture(LocalTime.of(12, 34), "4BU2", 19, "Trondheim", 9, LocalTime.of(3, 5));
+    departure = new TrainDeparture(
+        LocalTime.of(12, 34),
+        "4BU2",
+        19,
+        "Trondheim",
+        9,
+        LocalTime.of(3, 5)
+    );
   }
 
   @AfterEach
@@ -24,85 +30,134 @@ class TrainDepartureTest {
 
   @Test
   void constructor() {
-    assertDoesNotThrow(() ->
-        new TrainDeparture(
+    assertDoesNotThrow(
+        () -> new TrainDeparture(
             LocalTime.of(12, 34),
             "4BU2",
             19,
             "Trondheim",
             9,
             LocalTime.of(3, 5)
-        )
+        ),
+        "Expected no exception"
     );
-    assertThrows(InvalidParameterException.class, () ->
-        new TrainDeparture(
-            null,
-            "4BU2",
-            19,
-            "Trondheim",
-            9,
-            LocalTime.of(3, 3)
-        )
-    );
-    assertThrows(InvalidParameterException.class, () ->
-        new TrainDeparture(
+    TestHelper.assertThrowsWithMessage(
+        IllegalArgumentException.class,
+        () -> new TrainDeparture(
             LocalTime.of(12, 34),
             null,
             19,
             "Trondheim",
             9,
             LocalTime.of(3, 5)
-        )
+        ),
+        "Line cannot be null",
+        "Expected thrown exception when line is null"
     );
-    assertDoesNotThrow(() ->
-        new TrainDeparture(
+    TestHelper.assertThrowsWithMessage(
+        IllegalArgumentException.class,
+        () -> new TrainDeparture(
+            LocalTime.of(12, 34),
+            "A",
+            19,
+            "Trondheim",
+            9,
+            LocalTime.of(3, 5)
+        ),
+        "Line must be a string with a length between 2 and 7 characters.",
+        "Expected thrown exception when line name is smaller than 2 characters"
+    );
+    TestHelper.assertThrowsWithMessage(
+        IllegalArgumentException.class,
+        () -> new TrainDeparture(
+            LocalTime.of(12, 34),
+            "aa",
+            19,
+            "Trondheim",
+            9,
+            LocalTime.of(3, 5)
+        ),
+        "Line can only contain capital letters and numbers",
+        "Expected thrown exception when line "
+            + "doesnt contain only capital letters or numbers"
+    );
+    TestHelper.assertThrowsWithMessage(
+        IllegalArgumentException.class,
+        () -> new TrainDeparture(
+            LocalTime.of(12, 34),
+            "ABCDEZDE",
+            19,
+            "Trondheim",
+            9,
+            LocalTime.of(3, 5)
+        ),
+        "Line must be a string with a length between 2 and 7 characters.",
+        "Expected thrown exception when line name is longer than 7 characters"
+    );
+    TestHelper.assertThrowsWithMessage(
+        IllegalArgumentException.class,
+        () -> new TrainDeparture(
+            LocalTime.of(12, 34),
+            "4BU2",
+            0,
+            "Trondheim",
+            9,
+            LocalTime.of(3, 5)
+        ),
+        "Train number cannot be less than 1",
+        "Expected thrown exception when train number is smaller than 1"
+    );
+    TestHelper.assertThrowsWithMessage(
+        IllegalArgumentException.class,
+        () -> new TrainDeparture(
             LocalTime.of(12, 34),
             "4BU2",
             -1,
             "Trondheim",
             9,
             LocalTime.of(3, 5)
-        )
+        ),
+        "Train number cannot be less than 1",
+        "Expected thrown exception when train number is smaller than 1"
     );
-    assertThrows(InvalidParameterException.class, () ->
-        new TrainDeparture(
-            LocalTime.of(12, 34),
-            "4BU2",
-            -2,
-            "Trondheim",
-            9,
-            LocalTime.of(3, 5)
-        )
-    );
-    assertThrows(InvalidParameterException.class, () ->
-        new TrainDeparture(
+    TestHelper.assertThrowsWithMessage(
+        IllegalArgumentException.class,
+        () -> new TrainDeparture(
             LocalTime.of(12, 34),
             "4BU2",
             -1000,
             "Trondheim",
             9,
             LocalTime.of(3, 5)
-        )
+        ),
+        "Train number cannot be less than 1",
+        "Expected thrown exception when train number is smaller than 1"
     );
-    assertThrows(InvalidParameterException.class, () ->
-        new TrainDeparture(
+    TestHelper.assertThrowsWithMessage(
+        IllegalArgumentException.class,
+        () -> new TrainDeparture(
             LocalTime.of(12, 34),
             "4BU2",
             19,
             null,
             9,
             LocalTime.of(3, 5)
-        )
+        ),
+        "Destination cannot be null",
+        "Expected thrown exception when destination is null"
     );
-    assertThrows(InvalidParameterException.class, () ->
-        new TrainDeparture(
+    TestHelper.assertThrowsWithMessage(
+        IllegalArgumentException.class,
+        () -> new TrainDeparture(
             LocalTime.of(12, 34),
             "4BU2",
             19,
             "Trondheim",
             9,
             null
-        )
+        ),
+        "Delay cannot be null",
+        "Expected thrown exception when delay is null"
     );
   }
 
@@ -151,7 +206,7 @@ class TrainDepartureTest {
 
   @Test
   void getTrack() {
-    // not really required
+    // TODO not really required
     assertNotEquals(-1, departure.getTrack());
     assertEquals(9, departure.getTrack());
   }
@@ -160,10 +215,22 @@ class TrainDepartureTest {
   void setTrack() {
     departure.setTrack(10);
     assertEquals(10, departure.getTrack());
-    departure.setTrack(0);
-    assertEquals(0, departure.getTrack());
+    TestHelper.assertThrowsWithMessage(
+        IllegalArgumentException.class,
+        () -> departure.setTrack(0),
+        "Track id must be greater than 0, or -1 if undefined",
+        "Expected to not be able to set track to 0"
+    );
+    assertEquals(10, departure.getTrack());
     // TODO add test for error message
-    assertThrows(InvalidParameterException.class, () -> departure.setTrack(-1));
+    TestHelper.assertThrowsWithMessage(
+        IllegalArgumentException.class,
+        () -> departure.setTrack(-2),
+        "Track id must be greater than 0, or -1 if undefined",
+        "Expected to not be able to set track to a negative number that isnt -1"
+    );
+    departure.setTrack(-1);
+    assertEquals(-1, departure.getTrack());
   }
 
   @Test
@@ -185,15 +252,29 @@ class TrainDepartureTest {
     assertEquals(LocalTime.of(1, 2), departure.getDelay());
     departure.setDelay(LocalTime.MIN);
     assertEquals(LocalTime.MIN, departure.getDelay());
-    assertThrows(InvalidParameterException.class, () -> departure.setDelay(null));
+    TestHelper.assertThrowsWithMessage(
+        IllegalArgumentException.class,
+        () -> departure.setDelay(null),
+        "Delay cannot be null",
+        "Expected to not be able to set delay to null"
+    );
   }
 
   @Test
   void testToString() {
-    assertEquals("4BU2 til Trondheim (Tog #19): Planlagt avgang 12:34, forsinket til 15:39 fra spor 9", departure.toString());
+    assertEquals(
+        "4BU2 (Tog #19) til Trondheim: Planlagt kl 12:34, men forsinket til 15:39 fra spor 19",
+        departure.toString()
+    );
     departure.setDelay(LocalTime.MIN);
-    assertEquals("4BU2 til Trondheim (Tog #19): Planlagt avgang 12:34, med ingen forsinkelse fra spor 9", departure.toString());
-    // departure.setTrack(-1);
-    // TODO add test for -1
+    assertEquals(
+        "4BU2 (Tog #19) til Trondheim: Planlagt kl 12:34 med ingen forsinkelse fra spor 19",
+        departure.toString()
+    );
+    departure.setTrack(-1);
+    assertEquals(
+        "4BU2 (Tog #19) til Trondheim: Planlagt kl 12:34 med ingen forsinkelse uten satt spor",
+        departure.toString()
+    );
   }
 }
--- src/test/java/edu/ntnu/stud/TrainGroupTest.java
+++ src/test/java/edu/ntnu/stud/TrainGroupTest.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintStream;
 import java.nio.charset.Charset;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.NoSuchElementException;
@@ -15,41 +16,144 @@ import java.util.NoSuchElementException;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TrainGroupTest {
-  public static void main(String[] args) {
-    // TODO do i really need to test user input
+  private TrainGroup departures;
 
+  @BeforeEach
+  void setUp() {
+    departures = new TrainGroup();
+    departures.addDeparture(new TrainDeparture(
+        LocalTime.of(12, 0),
+        "A1",
+        1,
+        "Oslo"
+    ));
+    departures.addDeparture(new TrainDeparture(
+        LocalTime.of(12, 30),
+        "B2",
+        2,
+        "Trondheim"
+    ));
+    departures.addDeparture(new TrainDeparture(
+        LocalTime.of(9, 0),
+        "C3",
+        3,
+        "Trondheim"
+    ));
   }
 
   @AfterEach
   void tearDown() {
-    TestHelper.tearDown();
+    departures = null;
   }
 
   @Test
   void addDeparture() {
-    //TestHelper.setupMockInput("1", "2", "3");
-    //TestHelper.setupMockOutput("test:", "testa:");
-    //TrainGroup trainGroup = new TrainGroup();
-    //trainGroup.addDeparture();
+    TestHelper.assertThrowsWithMessage(
+        IllegalArgumentException.class,
+        () -> departures.addDeparture(null),
+        "Argument for @NotNull parameter 'departure' of edu/ntnu/stud/TrainGroup.addDeparture must not be null",
+        "Trying to add null to the group should throw a IllegalArgumentException"
+    );
+    TrainDeparture departure = new TrainDeparture(
+        LocalTime.of(16, 0),
+        "D4",
+        2,
+        "Bergen"
+    );
+    TestHelper.assertThrowsWithMessage(
+        IllegalArgumentException.class,
+        () -> departures.addDeparture(new TrainDeparture(
+            LocalTime.of(16, 0),
+            "D4",
+            2,
+            "Bergen"
+        )),
+        "Train number already in use",
+        "Trying to add a departure with a train number that is already in use should throw"
+    );
   }
 
   @Test
   void getDepartureFromNumber() {
+    TrainDeparture departure1 = departures.getDepartureFromNumber(1);
+    assertEquals(
+        "Oslo",
+        departure1.getDestination(),
+        "Expected destination to be Oslo"
+    );
+    TrainDeparture departure2 = departures.getDepartureFromNumber(2);
+    assertEquals(
+        "Trondheim",
+        departure2.getDestination(),
+        "Expected destination to be Trondheim"
+    );
+    TrainDeparture departure3 = departures.getDepartureFromNumber(3);
+    assertEquals(
+        "C3",
+        departure3.getLine(),
+        "Expected line to be C3"
+    );
+    assertNull(departures.getDepartureFromNumber(4), "Expected null when train number is not in use");
   }
 
   @Test
   void getDepartureFromDestination() {
+    TrainDeparture[] osloDepartures = departures.getDepartureFromDestination("Oslo");
+    assertEquals(1, osloDepartures.length, "Expected only one departure to Oslo");
+    TrainDeparture[] trondheimDepartures = departures.getDepartureFromDestination("Trondheim");
+    assertEquals(
+        2,
+        trondheimDepartures.length,
+        "Expected two departure to Trondheim"
+    );
+    assertEquals("B2", trondheimDepartures[0].getLine(), "Expected line to be B2");
+    assertEquals("C3", trondheimDepartures[1].getLine(), "Expected line to be C3");
+    assertEquals(
+        0,
+        departures.getDepartureFromDestination("Bergen").length,
+        "Expected no departures to Bergen"
+    );
+    TestHelper.assertThrowsWithMessage(
+        IllegalArgumentException.class,
+        () -> departures.getDepartureFromDestination(null),
+        "Argument for @NotNull parameter 'destination' of " +
+            "edu/ntnu/stud/TrainGroup.getDepartureFromDestination must not be null",
+        "Trying to get departures with destination null should throw"
+    );
   }
 
   @Test
   void getDeparturesFromTime() {
+    TrainDeparture[] departuresFromNine = departures.getDeparturesFromTime(
+        LocalTime.of(11, 59)
+    );
+    assertEquals(2, departuresFromNine.length, "Expected two departures");
+    TrainDeparture[] departuresFromTwelve = departures.getDeparturesFromTime(
+        LocalTime.of(12, 00)
+    );
+    assertEquals(1, departuresFromTwelve.length, "Expected only one departure");
+    TestHelper.assertThrowsWithMessage(
+        IllegalArgumentException.class,
+        () -> departures.getDeparturesFromTime(null),
+        "Argument for @NotNull parameter 'time' of " +
+            "edu/ntnu/stud/TrainGroup.getDeparturesFromTime must not be null",
+        "Trying to get departures from time null should throw"
+    );
   }
 
   @Test
-  void testGetDeparturesFromTime() {
+  @DisplayName("Name") // TODO add to all
+  void length() {
+    assertEquals(3, departures.length(), "Expected three departures");
   }
 
   @Test
   void removePassedDepartures() {
+    assertEquals(3, departures.length(), "Expected three departures");
+    departures.removePassedDepartures(LocalTime.of(11, 59));
+    assertEquals(2, departures.length(), "Expected two departures");
+    departures.removePassedDepartures(LocalTime.of(12, 0));
+    assertEquals(1, departures.length(), "Expected one departure");
+
   }
 }
