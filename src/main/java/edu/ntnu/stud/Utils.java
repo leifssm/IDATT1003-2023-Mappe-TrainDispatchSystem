662e9694cef559a5242e684d549198358e9e7b66
Leif Mørstad <leif@moerstad.no>
2023-11-29 14:48:15 +0100

    added docs for utils

--- src/main/java/edu/ntnu/stud/Utils.java
+++ src/main/java/edu/ntnu/stud/Utils.java
@@ -3,9 +3,24 @@ package edu.ntnu.stud;
 import java.util.regex.Pattern;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * This class contains utility methods that are used in multiple places in the application.
+ */
 public class Utils {
+  /**
+   * Pattern for validating train line names.
+   */
   public static final Pattern trainLinePattern = Pattern.compile("^[A-Z0-9]{2,7}$");
 
+  /**
+   * Pads the given string evenly on both sides so that the string end up at least with a length
+   * equal to the given number.
+   *
+   * @param string The string to pad
+   * @param padding The expected minimum length of the returned string after padding
+   * @return String with padding
+   * @throws IllegalArgumentException If given a null instead of a string
+   */
   public static @NotNull String padCenter(
       @NotNull String string,
       int padding
@@ -19,10 +34,20 @@ public class Utils {
     return " ".repeat(leftPadding) + string + " ".repeat(rightPadding);
   }
 
+  /**
+   * Shorthand for {@link Utils#padCenter(String, int)}, except that it accepts an integer.
+   *
+   * @see Utils#padCenter(String, int)
+   */
   public static @NotNull String pc(int n, int padding) {
     return padCenter(String.valueOf(n), padding);
   }
 
+  /**
+   * Shorthand for {@link Utils#padCenter(String, int)}.
+   *
+   * @see Utils#padCenter(String, int)
+   */
   public static @NotNull String pc(@NotNull String string, int padding) {
     return padCenter(string, padding);
   }
