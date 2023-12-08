b590dc01d1ebbfbd30a71acf8df77e4819f48528
Leif Mørstad <leif@moerstad.no>
2023-12-08 22:14:27 +0100

    added comments and improved structure of InputParser tests to clearer show the arrange, act, and assert pattern of testing

--- src/test/java/edu/ntnu/stud/InputParserTest.java
+++ src/test/java/edu/ntnu/stud/InputParserTest.java
@@ -1,8 +1,11 @@
 package edu.ntnu.stud;
 
 import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.time.LocalTime;
 import java.util.regex.Pattern;
+
+import org.intellij.lang.annotations.RegExp;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -22,15 +25,22 @@ class InputParserTest {
     @Test
     @DisplayName("isInitialized() returns if the parser is initialized")
     void isInitializedReturnIfInputParserIsInitialized() {
+      // Arrange
+      // Assert - Shouldn't be twice, but in this case it should check to not get a false positive
       assertFalse(InputParser.isInitialized());
+      // Act
       InputParser.initialize(new ByteArrayInputStream("".getBytes()));
+      // Assert
       assertTrue(InputParser.isInitialized());
     }
 
     @Test
     @DisplayName("getString() returns an unvalidated string from the user")
     void getStringReturnsAStringFromUser() {
+      // Arrange
       TestHelper.setupMockInput("test", "test2", "hello");
+      // Act
+      // Assert
       assertEquals("test", InputParser.getString());
       assertEquals("test2", InputParser.getString());
       assertEquals("hello", InputParser.getString());
@@ -39,21 +49,31 @@ class InputParserTest {
     @Test
     @DisplayName("getString() can be set to validate the input with a regex")
     void getStringRegexWithValidation() {
+      // Arrange
       TestHelper.setupMockInput("teST", "test2", "hello");
-      assertEquals("hello", InputParser.getString("Test", "[a-z]+"));
+      String prompt = "Test";
+      @RegExp String regexValidation = "[a-z]+";
+      // Act
+      // Assert
+      assertEquals("hello", InputParser.getString(prompt, regexValidation));
     }
 
     @Test
     @DisplayName("getString() can be set to validate the input with a pattern")
     void getStringPatternWithValidation() {
+      // Arrange
       TestHelper.setupMockInput("teST", "test2", "hello");
+      String prompt = "Test";
       Pattern pattern = Pattern.compile("[a-z]+");
-      assertEquals("hello", InputParser.getString("Test", pattern));
+      // Act
+      // Assert
+      assertEquals("hello", InputParser.getString(prompt, pattern));
     }
 
     @Test
     @DisplayName("getInt() returns an int from the user")
     void getInt() {
+      // Arrange
       TestHelper.setupMockInput(
           "",
           " ",
@@ -68,85 +88,121 @@ class InputParserTest {
           "2147483647",
           "-1"
       );
-      assertEquals(-2147483648, InputParser.getInt("Test"));
-      assertEquals(2147483647, InputParser.getInt("Test"));
-      assertEquals(-1, InputParser.getInt("Test"));
+      String prompt = "Test";
+      // Act
+      // Assert
+      assertEquals(-2147483648, InputParser.getInt(prompt));
+      assertEquals(2147483647, InputParser.getInt(prompt));
+      assertEquals(-1, InputParser.getInt(prompt));
     }
 
     @Test
     @DisplayName("getInt() can be set to validate the input")
     void getIntWithValidation() {
+      // Arrange
       TestHelper.setupMockInput("", " ", "en", "to", "-", "-1", "0", "1");
-      assertEquals(1, InputParser.getInt("Test", n -> n > 0));
+      String prompt = "Test";
+      // Act
+      // Assert
+      assertEquals(1, InputParser.getInt(prompt, n -> n > 0));
     }
 
     @Test
     @DisplayName("getFloat() returns a float from the user")
     void getFloat() {
+      // Arrange
       TestHelper.setupMockInput("", " ", "en", "to", "-", "-0.1", "0.1", "1.9", "2");
-      assertEquals(-0.10000000149011612, InputParser.getFloat("Test"));
-      assertEquals(0.10000000149011612, InputParser.getFloat("Test"));
-      assertEquals(1.899999976158142, InputParser.getFloat("Test"));
-      assertEquals(2, InputParser.getFloat("Test"));
+      String prompt = "Test";
+      // Act
+      // Assert
+      assertEquals(-0.10000000149011612, InputParser.getFloat(prompt));
+      assertEquals(0.10000000149011612, InputParser.getFloat(prompt));
+      assertEquals(1.899999976158142, InputParser.getFloat(prompt));
+      assertEquals(2, InputParser.getFloat(prompt));
     }
 
     @Test
     @DisplayName("getFloat() can be set to validate the input")
     void getFloatWithValidation() {
+      // Arrange
       TestHelper.setupMockInput("", " ", "en", "to", "-", "-0.1", "0.1", "1.9", "2", "7", "8");
-      assertEquals(8, InputParser.getFloat("Test", n -> n > 7));
+      String prompt = "Test";
+      // Act
+      // Assert
+      assertEquals(8, InputParser.getFloat(prompt, n -> n > 7));
     }
 
     @Test
     @DisplayName("getChar() returns an int from the user")
     void getChar() {
+      // Arrange
       TestHelper.setupMockInput("", "das", "ds", "FSFA", "A", "d", "+/", "/", "7", "  ", " ");
-      assertEquals('A', InputParser.getChar("Test"));
-      assertEquals('d', InputParser.getChar("Test"));
-      assertEquals('/', InputParser.getChar("Test"));
-      assertEquals('7', InputParser.getChar("Test"));
-      assertEquals(' ', InputParser.getChar("Test"));
+      String prompt = "Test";
+      // Act
+      // Assert
+      assertEquals('A', InputParser.getChar(prompt));
+      assertEquals('d', InputParser.getChar(prompt));
+      assertEquals('/', InputParser.getChar(prompt));
+      assertEquals('7', InputParser.getChar(prompt));
+      assertEquals(' ', InputParser.getChar(prompt));
     }
 
     @Test
     @DisplayName("getChar() can be set to validate the input")
     void getCharWithValidation() {
+      // Arrange
       TestHelper.setupMockInput("", "das", "ds", "FSFA", "A", "B", "C", "D", "E", "FF", "G");
-      assertEquals('G', InputParser.getChar("Test", n -> n > 'E'));
+      String prompt = "Test";
+      // Act
+      // Assert
+      assertEquals('G', InputParser.getChar(prompt, n -> n > 'E'));
     }
 
     @Test
     @DisplayName("getTime() returns a LocalTime from the user")
     void getTime() {
+      // Arrange
       TestHelper.setupMockInput("", "2", "12:0", "0:60", "000:00", "24:00", "4:00", "23:59", "04:00");
-      assertEquals(LocalTime.of(4, 0), InputParser.getTime("Test"));
-      assertEquals(LocalTime.of(23, 59), InputParser.getTime("Test"));
-      assertEquals(LocalTime.of(4, 0), InputParser.getTime("Test"));
+      String prompt = "Test";
+      // Act
+      // Assert
+      assertEquals(LocalTime.of(4, 0), InputParser.getTime(prompt));
+      assertEquals(LocalTime.of(23, 59), InputParser.getTime(prompt));
+      assertEquals(LocalTime.of(4, 0), InputParser.getTime(prompt));
     }
 
     @Test
     @DisplayName("getTime() can be set to validate the input")
     void getTimeWithValidation() {
+      // Arrange
       TestHelper.setupMockInput("4:00", "23:59", "14:00");
+      String prompt = "Test";
+      // Act
+      // Assert
       assertEquals(
           LocalTime.of(14, 0),
-          InputParser.getTime("Test", t -> t.getHour() > 12 && t.getHour() < 20)
+          InputParser.getTime(prompt, t -> t.getHour() > 12 && t.getHour() < 20)
       );
     }
 
     @Test
     @DisplayName("getBoolean() returns a boolean from the user")
     void getBoolean() {
+      // Arrange
       TestHelper.setupMockInput("", "das", "ds", "FSF", "A", "B", "Y", "n", "E", "FF", "G", "N", "y");
-      assertTrue(InputParser.getBoolean("Test"));
-      assertFalse(InputParser.getBoolean("Test"));
-      assertFalse(InputParser.getBoolean("Test"));
-      assertTrue(InputParser.getBoolean("Test"));
+      String prompt = "Test";
+      // Act
+      // Assert
+      assertTrue(InputParser.getBoolean(prompt));
+      assertFalse(InputParser.getBoolean(prompt));
+      assertFalse(InputParser.getBoolean(prompt));
+      assertTrue(InputParser.getBoolean(prompt));
     }
 
     @Test
     @DisplayName("getBoolean() can return a default value if the user does not pass anything")
     void getBooleanWithDefault() {
+      // Arrange
       TestHelper.setupMockInput(
           "da",
           "ds",
@@ -163,17 +219,24 @@ class InputParserTest {
           "HE",
           ""
       );
-      assertTrue(InputParser.getBoolean("Test", false));
-      assertTrue(InputParser.getBoolean("Test", true));
-      assertFalse(InputParser.getBoolean("Test", false));
+      String prompt = "Test";
+      // Act
+      // Assert
+      assertTrue(InputParser.getBoolean(prompt, false));
+      assertTrue(InputParser.getBoolean(prompt, true));
+      assertFalse(InputParser.getBoolean(prompt, false));
     }
 
     @Test
     @DisplayName("waitForUser() waits for the user to enter any value")
     void waitForUser() {
+      // Arrange
+      String prompt = "Press enter if you want to continue the program";
+      // Act
+      // Assert
       TestHelper.setupMockInput("", "", "any string", " ");
       InputParser.waitForUser();
-      InputParser.waitForUser("Press enter if you want to continue the program");
+      InputParser.waitForUser(prompt);
       InputParser.waitForUser();
       InputParser.waitForUser();
     }
@@ -185,9 +248,13 @@ class InputParserTest {
     @Test
     @DisplayName("initialize() throws when trying to initialize with null")
     void initializeThrowsWithNull() {
+      // Arrange
+      InputStream stream = null;
+      // Act
+      // Assert
       TestHelper.assertThrowsWithMessage(
           IllegalArgumentException.class,
-          () -> InputParser.initialize(null),
+          () -> InputParser.initialize(stream),
           "Argument for @NotNull parameter 'stream' of "
               + "edu/ntnu/stud/InputParser.initialize must not be null",
           "InputParser should not be able to be initialized with a null stream"
